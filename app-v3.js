22dd366147a6bcc21bc2dd27aaf2170bc9d9b370
trancanh1102 <40576239+trancanh1102@users.noreply.github.com>
2020-11-09 02:50:24 +0700

    Update app-v3.js

--- app-v3.js
+++ app-v3.js
@@ -26,9 +26,11 @@ function init() {
   var html = `
 <header class="nexmoe-nav">
 	<div class="navSize">
-			<a href="/"><img class="avatar" src="https://i.pinimg.com/564x/44/c2/0a/44c20a3032967f24ee917c25839c472e.jpg"/></a>
+			<a href="/"><img class="avatar" src="https://i.loli.net/2020/04/24/wCxMZFtiv6aKmQ1.png"/></a>
 			<div class="nav_menu">
 				<ul class="menu_ul">
+					<li class="menu_li"><a href="https://5mayrain.github.io/" target="_blank">博客</a></li>
+					<li class="menu_li"><a href="https://drive.google.com/" target="_blank">登陆</a></li>
 				</ul>
 				<div class="nav_icon" ></div>
 			</div>
